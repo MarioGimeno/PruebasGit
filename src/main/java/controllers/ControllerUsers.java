0b5626b6196b5a5d1311fc0fcab7dc1ec7c65654
mozv <mozv@gft.com>
2024-10-07 16:01:50 +0200

    Commit controller

--- src/main/java/controllers/ControllerUsers.java
+++ src/main/java/controllers/ControllerUsers.java
@@ -1,8 +1,22 @@
 package controllers;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class ControllerUsers {
 
-    private String nombre;
+    ServiceController serviceController = new ServiceController();
+    @GetMapping("/")
+    public String getAllUsers(){
+        return serviceController.getAllUsers();
+    }
 
 
 }
