c0686874f6eb50a8c7f1cfd52d08710b8f19acc1
Mario Gimeno Navaz <mozv@gft.com>
2024-10-09 15:59:00 +0200

    Primer commit prueba 2

--- src/main/java/repositories/RepositoryUser.java
+++ src/main/java/repositories/RepositoryUser.java
@@ -1,12 +1,10 @@
 package repositories;
 
-import com.example.demo.Entities.User;
+import entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RepositoryUser extends JpaRepository<User, Long> {
-
-    //uuooooolaaa
-    //hola2
+   String nombre = "Marta";
 }
